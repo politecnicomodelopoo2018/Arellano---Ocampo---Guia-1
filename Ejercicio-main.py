dea8ecee2c8d0d13e7afbe717c59f9ad114c6cb9
jorge-arellan <arellano.ariel290@gmail.com>
2018-04-27 09:42:56 -0300

    Clase del 27/04

--- Ejercicio-main.py
+++ Ejercicio-main.py
@@ -4,10 +4,10 @@ from Ejercicio7 import *
 menu = True
 opcion = 0
 opcionB = 0
-Sis = Colegio()
+bufet = Bufet()
+Sis = Colegio(bufet)
 temp = None
-
-while menu:
+while opcion != "5":
     print("Menu:")
     print("1- Alumno")
     print("2- Profesor")
@@ -17,24 +17,28 @@ while menu:
 
     opcion = input()
 
-    while opcion == 1:
+    while opcion == "1":
         print("1- Añadir")
         print("2- Modificar")
         print("3- Eliminar")
+        print("4- Volver")
 
         opcionB = input()
 
-        if opcionB == 1:
+        if opcionB == "1":
             CrearAlumno(Sis)
 
-        if opcionB == 2:
+        if opcionB == "2":
             print("Ingrese el alumno a modificar")
             temp = input()
-            ModificarAlumno(Sis, temp)
+            print("HI")
+            ModificarPersona(Sis, temp)
+
+        if opcionB == "4":
+            opcion = 0
 
 
 
 
 
-    if opcion == 5:
-        menu = False
+
--- Ejercicio7.py
+++ Ejercicio7.py
@@ -35,8 +35,20 @@ class Pedido (object):
         self.horaDeEntrega = horaDeEntrega
         self.entregado = False
 
-    def entregado (self):
-        self.engregado = True
+    def setFechaCreacion (self, fecha):
+        self.fechaCreacion = fecha
+
+    def setPlato (self, plato):
+        self.plato = plato
+
+    def setPersona (self, persona):
+        self.persona = persona
+
+    def setHoraEntrega (self, hora):
+        self.horaDeEntrega = hora
+
+    def setEntregado (self, entregado):
+        self.engregado = entregado
 
 class Bufet (object):
 
@@ -73,9 +85,105 @@ def CrearAlumno (Sis):
     A.setDivision(input())
     Sis.listaPersonas.append(A)
 
-def ModificarAlumno (Sis, nombre):
-    for item in Sis:
+def ModificarPersona (sis, nombre):
+    print("IN")
+    for item in sis.listaPersonas:
+        print("Atroden")
         if item.nombre == nombre:
+            while item is Alumno and A != 4:
+                print("1-Modificar Nombre")
+                print("2-Modificar Apellido")
+                print("3-Modificar Division")
+                print("4-Salir")
+                A = input()
+                if A == 1:
+                    print("Nombre nuevo")
+                    B = input()
+                    item.nombre.setNombre(B)
+                if A == 2:
+                    print("Apellido nuevo")
+                    B = input()
+                    item.nombre.setApellido(B)
+                if A == 3:
+                    print("Division nuva")
+                    B = input()
+                    item.nombre.setDivision(B)
+
+            while item is Profesor and A != 4:
+                print("1-Modificar Nombre")
+                print("2-Modificar Apellido")
+                print("3-Modificar Descuento")
+                print("4-Salir")
+                A = input()
+                if A == 1:
+                    print("Nombre nuevo")
+                    B = input()
+                    item.setNombre(B)
+                if A == 2:
+                    print("Apellido nuevo")
+                    B = input()
+                    item.setApellido(B)
+                if A == 3:
+                    print("Descuento nuevo")
+                    B = input()
+                    item.setDescuento(B)
+
+def ModificarPlato (sis, nombre):
+    for item in sis.bufet.listaPlatos:
+        if item.nombre == nombre:
+            while A != 3:
+                print("1-Modificar nombre")
+                print("2-Modificar precio")
+                print("3-Salir")
+                A = input()
+                if A == 1:
+                    print("Nombre nuevo")
+                    B = input()
+                    item.setNombre(B)
+                if A == 2:
+                    print("Precio nuevo")
+                    B = input()
+                    item.setPrecio(B)
+
+def ModificarPedido (sis, persona):
+    for item in sis.bufet.listaPedidos:
+        if item.persona == persona:
+            while A != 6:
+                print("1-Modificar fechaCreacion")
+                print("2-Modificar plato")
+                print("3-Modificar persona")
+                print("4-Modificar horaEntrega")
+                print("5-Modificar entregado")
+                print("6-Salir")
+                A = input()
+                if A == 1:
+                    print("FechaCreacion nueva")
+                    B = input()
+                    item.setFechaCreacion(B)
+                if A == 2:
+                    print("Plato nuevo")
+                    B = input()
+                    item.setPlato(B)
+                if A == 3:
+                    print("Persona nueva")
+                    B = input()
+                    for met in sis.listaPersonas:
+                        if met == B:
+                            item.setPersona = met
+                if A == 4:
+                    print("Hora nueva")
+                    B = input()
+                    item.setHoraEntrega(B)
+                if A == 5:
+                    print("1-Entregado")
+                    print("2-No entregado")
+                    B = input()
+                    if B == 1:
+                        item.setEntregado(True)
+                    if B == 2:
+                        item.setEntregado(False)
+
+
 
 
 
